c512e1dcce761fddc3b2a4343dfcc96c85a596f2
Ju4n3sD3v <juanherrera@unal.edu.co>
2025-02-15 17:47:46 -0500

    Añadido el menú, falta darle estilo

--- src/gestorAplicacion/uiMain/ventanaInicio.py
+++ src/gestorAplicacion/uiMain/ventanaInicio.py
@@ -1,9 +1,27 @@
 #interfaz de inicio 
 
 import tkinter as tk
+from tkinter import messagebox
 
 ventanaPrincipal = tk.Tk()
 ventanaPrincipal.geometry("500x300")
+
+#Menú
+
+barraMenu = tk.Menu(ventanaPrincipal)
+ventanaPrincipal.config(menu=barraMenu)
+
+menu1 = tk.Menu(barraMenu)
+barraMenu.add_cascade(label="Inicio", menu=menu1)
+menu1.add_command(label="Salir de la aplicación", command=ventanaPrincipal.quit)
+menu1.add_separator()
+
+def info():
+    messagebox.showinfo("Información", "Este sistema fue desarrollado por el grupo JJAYC, \n")
+
+menu1.add_command(label="Descripción del sistema", command=info)
+
+#Frame superior
 
 frame_superior = tk.Frame(ventanaPrincipal)
 frame_superior.pack(fill="both", expand=True)
@@ -28,8 +46,10 @@ etiqueta2 = tk.Label(
 )
 etiqueta2.pack(side="right", fill="both", expand=True, padx=10, pady=10)
 
+#Frame inferior
+
 frame_inferior = tk.Frame(ventanaPrincipal)
-frame_inferior.pack(fill="both", expand=True)
+frame_inferior.pack(fill="both", expand=True)   
 
 
 boton1 = tk.Button(frame_inferior, text="Presione para utilizar\nel sistema")
