e129897aa473a2836dba8d80f8b6332f64524b72
YHANJARAMILLOD <yhjaramillod@unal.edu.co>
2025-02-25 06:19:23 -0500

    FIN2
    falta lo de galvis

--- src/seleccionFuncionalidad.py
+++ src/seleccionFuncionalidad.py
@@ -453,10 +453,10 @@ class VentanaSecundaria(tk.Tk):
         transporte.abastecerProducto(tienda_seleccionada, self.productosGenerados)
         tienda_seleccionada.descargarProducto(transporte)
         #METAS DE PAGO TRABAJADORES
-        self.conductorSeleccionado.setIndiceMeta(self.conductorSeleccionado.getIndiceMeta() + self.pesoTotalProductos)
+        """self.conductorSeleccionado.setIndiceMeta(self.conductorSeleccionado.getIndiceMeta() + self.pesoTotalProductos)
         self.conductorSeleccionado.getCantidadTrabajo() += 1
         Fabrica.getOperario().setIndiceMeta(Fabrica.getOperario().getIndiceMeta() + 1)
-        Fabrica.getOperario().getCantidadTrabajo() += 1
+        Fabrica.getOperario().getCantidadTrabajo() += 1"""
         # Mostrar resumen final unificado
         self.limpiar_frame_interaccion()
         frame_resumen = tk.Frame(self.frame_interaccion)
